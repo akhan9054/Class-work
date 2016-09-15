5c2df918264a6ef464b725553c69d4bb594779df
akhan9054 <akhan9054@bths.edu>
2016-09-14 22:43:01 -0400

    Update ProceduralExample.java

--- Introduction_Ahmed/src/Introduction/ProceduralExample.java
+++ Introduction_Ahmed/src/Introduction/ProceduralExample.java
@@ -3,5 +3,6 @@ package Introduction;
 public class ProceduralExample {
 	public static void main(String args[]){
 		System.out.println("Hello");
+		System.out.println("The Fitnessgram Pacer Test is a multistage aerobic capacity test.");
 	}
 }
