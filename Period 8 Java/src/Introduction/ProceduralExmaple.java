5448d03ec8bf62a57509d9fb33677fbc5b454b9c
Student 8 <akhan9054@bths.edu>
2016-09-15 13:54:03 -0400

    Spacing

--- Period 8 Java/src/Introduction/ProceduralExmaple.java
+++ Period 8 Java/src/Introduction/ProceduralExmaple.java
@@ -5,6 +5,6 @@ public class ProceduralExmaple {
 		//System.out.println("Hello, world");
 		String[] names = {"Jillian", "Joseph", "Jordan"};
 		String[] years = {"Freshie", "Sophomore", "Senior"};
-		System.out.println("Hi, my name is " + names[0] + "and I'm a " + years[0] + ".");
+		System.out.println("Hi, my name is " + names[0] + " and I'm a " + years[0] + ".");
 	}
 }
