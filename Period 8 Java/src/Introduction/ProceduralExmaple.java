835b3ce994efcfe6d27f7cf6ace8d19ca0539b4b
Student 8 <akhan9054@bths.edu>
2016-09-16 12:40:42 -0400

    3

--- Period 8 Java/src/Introduction/ProceduralExmaple.java
+++ Period 8 Java/src/Introduction/ProceduralExmaple.java
@@ -6,5 +6,10 @@ public class ProceduralExmaple {
 		String[] names = {"Jillian", "Joseph", "Jordan"};
 		String[] years = {"Freshie", "Sophomore", "Senior"};
 		System.out.println("Hi, my name is " + names[0] + " and I'm a " + years[0] + ".");
+		System.out.println("Hi, my name is " + names[1] + " and I'm a " + years[1] + ".");
+		System.out.println("Hi, my name is " + names[2] + " and I'm a " + years[2] + ".");
+		/*for (){
+			
+		}*/
 	}
 }
