57e3d0cb1be0fa12c5d02cd30d23e55766ca1300
akhan9054 <akhan9054@bths.edu>
2016-09-20 23:10:01 -0400

    Update RandomCardPull.java

--- Period 8 Java/src/Introduction/RandomCardPull.java
+++ Period 8 Java/src/Introduction/RandomCardPull.java
@@ -3,15 +3,15 @@ package Introduction;
 public class RandomCardPull {
 	public static void main(String[] args) {
 		String[] suits = {"Spades", "Hearts", "Clubs", "Spades"};
-		
-		int cardInfo = pickRandomCard();
-		
+			
+		int cardInfo[] = pickRandomCard();
+			
 		System.out.println(cardInfo[0] + " of " + suits[cardInfo[1]]);
 	}
-	
-	public static int pickRandomCard(){
-		int rank = (Math.random()*13)+1;
-		int suit = (Math.random()*4)+1;
+		
+	public static int[] pickRandomCard(){
+		int rank = (int)((Math.random()*13)+1);
+		int suit = (int)((Math.random()*3)+1);
 		int[] cardArray = {rank, suit};
 		return cardArray;
 	}
